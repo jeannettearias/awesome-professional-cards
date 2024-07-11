bfe5fe9754aa7c13c43d584303bbcb6dc0f21039
jeannettearias <jeannettearias@icloud.com>
2024-07-11 18:44:03 +0200

    merge form-fill creation and add first elements to listen

---
+++ src/js/form-fill.js
@@ -0,0 +1,41 @@
+"use strict";
+
+//QUERY SELECTORS
+const inputName = document.querySelector('.js__input_name');
+const inputJob = document.querySelector('.js__input_job');
+
+
+
+//VARIABLES
+let data = {
+    pallete: '',
+    name: '',
+    job: '',
+    phone: '',
+    email: '',
+    linkedin: '',
+    github: '',
+    photo: '',
+
+};
+
+
+console.log(data);
+
+//FUNCTIONS AND EVENTS
+
+//handle the input name
+function handleInputName(ev) {
+    //listen the value input from property
+
+}
+
+handleInputName();
+
+//load the page 
+// API https://dev.adalab.es/api/card/
+// 
+//
+//
+/////////////////////////////////////
+
---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+'use strict'
+
+//Global configurations
+import ('./form-fill')
+
+//Global variables
+
+
