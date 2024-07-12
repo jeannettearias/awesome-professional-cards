981ae2cf2c602f0954e06d0c66e578c08a05b3bf
MVictoriaDoll <mvictoriadoll@gmail.com>
2024-07-12 19:28:01 +0200

    add disabled button

--- src/js/form-share.js
+++ src/js/form-share.js
@@ -1,11 +1,13 @@
 "use strict";
 
 const createCardBtn = document.querySelector('.js__createCardBtn');
+const createCardIcon = document.querySelector('.js__createCardIcon');
 
 const handleClickCreate = (ev) => {
     ev.preventDefault();
     console.log('boton clickeado');
     createCardBtn.classList.add('disabled');
+    createCardIcon.classList.add('disabled_icon');
 
 }
 
