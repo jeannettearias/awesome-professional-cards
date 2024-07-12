fdcfbfc6667fdd91961d784078f0481ab7029650
jeannettearias <jeannettearias@icloud.com>
2024-07-12 17:41:38 +0200

    add all listener events for input for form fields

--- src/js/form-fill.js
+++ src/js/form-fill.js
@@ -1,36 +1,99 @@
 "use strict";
 
 //QUERY SELECTORS
-const inputName = document.querySelector('.js__input_name');
-const inputJob = document.querySelector('.js__input_job');
+const nameInput = document.querySelector('.js__input_name');
+const jobInput = document.querySelector('.js__input_job');
+const phoneInput = document.querySelector('.js__input_phone');
+const emailInput = document.querySelector('.js__input_email');
+const linkedinInput = document.querySelector('.js__input_linkedin');
+const githubInput = document.querySelector('.js__input_github');
+
+const namePreview = document.querySelector('.js__namePreview');
+const jobPreview = document.querySelector('.js__jobPreview');
+const phonePreview = document.querySelector('.js__phonePreview');
+const emailPreview = document.querySelector('.js__emailPreview');
+const linkedinPreview = document.querySelector('.js__linkedinPreview');
+const githubPreview = document.querySelector('.js__githubPreview');
 
 
 
-//VARIABLES
+//GLOBAL VARIABLES
+
 let data = {
-    pallete: '',
+    //   palette: 1,
     name: '',
     job: '',
-    phone: '',
+    photo: '',
     email: '',
+    phone: '',
     linkedin: '',
     github: '',
-    photo: '',
 
 };
 
-
-console.log(data);
-
 //FUNCTIONS AND EVENTS
 
-//handle the input name
-function handleInputName(ev) {
-    //listen the value input from property
+//listen and display the NAME input by User
+const handleInputName = (ev) => {
+    //const value = nameInput.value;
+    //namePreview.innerHTML = value;
 
-}
+    data.name = nameInput.value;
+    namePreview.innerHTML = data.name;
 
-handleInputName();
+
+};
+nameInput.addEventListener('input', handleInputName);
+
+//listen and display the JOB input by User
+const handleInputJob = (ev) => {
+
+    data.job = jobInput.value;
+    jobPreview.innerHTML = data.job;
+
+};
+jobInput.addEventListener('input', handleInputJob);
+
+//listen the phone event and change the href link (js__phonePreview)
+const handleInputPhone = (ev) => {
+
+    data.phone = phoneInput.value;
+    phonePreview.innerHTML = data.phone;
+
+};
+jobInput.addEventListener('input', handleInputPhone);
+
+// listen the email event and change the href link (js__emailPreview)
+const handleInputEmail = (ev) => {
+
+    data.email = emailInput.value;
+    emailPreview.innerHTML = data.email;
+
+};
+jobInput.addEventListener('input', handleInputEmail);
+
+// listen the linkedin event and change the href link (js__linkedinPreview)
+const handleInputLinkedin = (ev) => {
+
+    data.linkedin = linkedinInput.value;
+    linkedinPreview.innerHTML = data.linkedin;
+
+};
+jobInput.addEventListener('input', handleInputLinkedin);
+
+// listen the github event and change the href link (js__githubPreview)
+const handleInputGithub = (ev) => {
+
+    data.github = githubInput.value;
+    githubPreview.innerHTML = data.github;
+
+};
+jobInput.addEventListener('input', handleInputGithub);
+
+
+
+
+
 
 //load the page 
 // API https://dev.adalab.es/api/card/
