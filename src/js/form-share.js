05917411069fafb24c67cfd83eb9087e309109b2
MVictoriaDoll <mvictoriadoll@gmail.com>
2024-07-13 15:44:01 +0200

    add fetch

--- src/js/form-share.js
+++ src/js/form-share.js
@@ -9,12 +9,38 @@ const fieldsetShare = document.querySelector(".js__form_fieldset_share");
 const formShareSection = document.querySelector(".js__form-share-section");
 const designForm = document.querySelector(".js__design_form");
 const formFill = document.querySelector(".js__form_fill");
+const createMessage = document.querySelector('.js__createMessage');
+const creatCardLink = document.querySelector('.js__creatCardLink');
 
 const handleClickCreate = (ev) => {
     ev.preventDefault();
     console.log("boton clickeado");
     createCardBtn.classList.add("disabled");
     createCardIcon.classList.add("disabled_icon");
+
+
+    fetch('https://dev.adalab.es/api/card/', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'content-type': 'aplication/json'
+        }
+    })
+
+    .then( response => response.json())
+    .then (dataResponse => {
+
+        if(dataResponse.success === false) {
+            createMessage.innertHTML = 
+            `<P>Ha sucedido un error al crear la tarjeta</P>
+             <P>${dataResponse.error}</P>
+            `;
+        } else if (dataResponse.success === true) {
+            creatCardLink.innerHTML = `${dataResponse.cardURL}`;
+
+        }
+    }) 
+
 };
 
 const handleclickDisplay = (ev) => {
